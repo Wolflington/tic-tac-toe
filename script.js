890914c17544283c58b89e723ee0547070a2d9cd
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-10-06 09:59:02 +0800

    Add module for Game and assign players

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Store every grid in an array inside of Gameboard object
-const gameboard = (() => {
+const Gameboard = (() => {
     //Create an array for board
     let board = ['', '', '', '', '', '', '', '', '']; //board where 'X' and 'O' goes
 
@@ -23,15 +23,56 @@ const gameboard = (() => {
     return { getBoard, getMarker, resetBoard };
 })();
 
-//Object for PLAYER
-const player = (name, marker) => {
+//Factory Function for PLAYER
+const Player = (name) => {
     const getName = () => {
         return name;
     }
-
-    const getMarker = () => {
-        return marker;
-    }
-
-    return {getName, getMarker};
+    return {getName};
 }
+
+const Game = (() => {
+    let currentPlayer;
+    let winner = null;
+    let isGameOver = false;
+
+    //Function expression to start the game and set the players
+    const startGame = (player1, player2) => {
+        currentPlayer = player1;
+        Gameboard.resetBoard(); //Inherit resetBoard from Gameboard module
+        isGameOver = false;
+        winner = null; //isGameOver and winner is set to default in every start of the game
+        // render() //Keep this function commented for a while since I still haven't thought of it
+    };
+
+    const getCurrentPlayer = () => currentPlayer; //getCurrentPlayer will be returned to swap players
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const checkForWinners = () => {
+
+    };
+
+    const makeMove = () => {
+        if (!isGameOver) {
+            if(Gameboard.getMarker(index, currentPlayer.marker)) { //Calls other functions to invoke to keep the game playing
+                checkForWinners();
+                switchPlayer();
+                // render();
+            }
+        }
+    };
+
+    // const render = () => { //Updates the UI to reflect the current state of the game
+
+    // };
+
+    return { startGame, getCurrentPlayer, makeMove };
+})();
+
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+
+Game.startGame(player1, player2);
